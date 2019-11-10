b49d1c8e62a8a63d7ef8f7582d7198c7bd348121
Inga Lovinde <52715130+inga-lovinde@users.noreply.github.com>
2019-11-10 17:30:56 +0300

    Template improved

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,6 +1,6 @@
 const baseSettings = {
     env: {
-        es6: true
+        es6: true,
     },
     extends: [
         "airbnb-base",
@@ -8,26 +8,28 @@ const baseSettings = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "plugin:@typescript-eslint/all",
-        "plugin:fp/recommended"
+        "plugin:fp/recommended",
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaVersion: 2018,
         project: "tsconfig-eslint.json",
-        sourceType: "module"
+        sourceType: "module",
     },
     plugins: [
         "@typescript-eslint",
-        "fp"
+        "fp",
     ],
     rules: {
+        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        "@typescript-eslint/no-type-alias": "off",
     },
     settings: {
         "import/resolver": {
             node: {
-                extensions: ['.ts']
-            }
-        }
+                extensions: ['.ts'],
+            },
+        },
     },
 };
 
@@ -39,9 +41,10 @@ module.exports = {
             files: ["*.spec.ts"],
             rules: {
                 ...baseSettings.rules,
+                "@typescript-eslint/no-magic-numbers": "off",
                 "fp/no-nil": "off",
-                "fp/no-unused-expression": "off"
-            }
-        }
-    ]
+                "fp/no-unused-expression": "off",
+            },
+        },
+    ],
 };
